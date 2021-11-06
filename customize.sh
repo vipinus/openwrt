e9f0b5652fa5cf0464dda81a64ed947e824c6122
vipinus <37501804+vipinus@users.noreply.github.com>
2021-11-06 13:19:22 -0400

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,4 +8,5 @@
 #1. Modify default IP
 cp -r files/* openwrt/
 chmod +x openwrt/package/base-files/files/etc/rc.local
+chmod +x openwrt/package/base-files/files/etc/crontabs/root
 chmod +x openwrt/package/base-files/files/sbin/*
