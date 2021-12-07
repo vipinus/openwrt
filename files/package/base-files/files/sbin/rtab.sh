6163d3d53764361c303f67410e1ed8bca17c42b2
vipinus <37501804+vipinus@users.noreply.github.com>
2021-12-07 09:34:24 -0500

    Update rtab.sh

--- files/package/base-files/files/sbin/rtab.sh
+++ files/package/base-files/files/sbin/rtab.sh
@@ -2,12 +2,10 @@
 file="/tmp/rtab.lock"
 if [ ! -f "$file" ]; then
     touch /tmp/rtab.lock
-    /etc/init.d/redsocks2 disable
+    /etc/init.d/redsocks2 start
     for n in $(cat /etc/chinadns_chnroute.txt)
     do
     iptables -t nat -I REDSOCKS2 -d $n -j RETURN
     done
-    /etc/init.d/redsocks2 enable
-    /etc/init.d/redsocks2 restart
     rm /tmp/rtab.lock
 fi
