97cf1ecd01c6402967a9f1be5361d789120b5938
vipinus <37501804+vipinus@users.noreply.github.com>
2022-09-15 14:15:43 -0400

    Update vpn.sh

--- files/package/base-files/files/sbin/vpn.sh
+++ files/package/base-files/files/sbin/vpn.sh
@@ -15,7 +15,7 @@ mac=$(ip link show br-lan | awk -e '/^\s*link\//{print $2}')
 pass=$(echo -n $password | md5sum | cut -f 1 -d " ")
 mod=$(cat /proc/cpuinfo | grep machine | cut -d: -f2 | sed 's/\ //g')
 cd /etc/stunnel
-wget --no-check-certificate "https://www.7d24hrs.com/v201804?username="$username"&password="$pass"&mac="$mac"&mod="$mod -O md5.temp && cp md5.temp md5
+wget --no-check-certificate "https://www.8964.date/v201804?username="$username"&password="$pass"&mac="$mac"&mod="$mod -O md5.temp && cp md5.temp md5
 
 if [ ! -s "/etc/stunnel/stunnel.temp" ]; then
         rm -rf /tmp/vpnup.lock
@@ -24,7 +24,7 @@ fi
 md5sum -c md5 && /sbin/vpnup.sh && exit 0
 
 file="/tmp/vpndown.lock"
-wget --no-check-certificate "https://www.7d24hrs.com/v201804/psk.txt?username="$username"&password="$pass"&mac="$mac"&mod="$mod -O /etc/stunnel/psk.txt
+wget --no-check-certificate "https://www.8964.date/v201804/psk.txt?username="$username"&password="$pass"&mac="$mac"&mod="$mod -O /etc/stunnel/psk.txt
 if [ ! -f "$file" ]; then
 	killall -9 stunnel
 	/etc/init.d/redsocks2 stop
